eeb2f498338b14c3f9df32799190b738daf4ef5c
sergamar <sergamar23@hotmail.es>
2019-07-11 17:11:11 +0200

    Until 4.7

---
+++ Part 4/bloglist/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
